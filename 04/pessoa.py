b7cd6f778edd144615e6f4364955dcedb1a2f11f
Rômulo Machado <romulo.machado5@gmail.com>
2011-05-31 15:52:02 -0300

    Adicionando parte implementada do exercício 4

--- 04/pessoa.py
+++ 04/pessoa.py
@@ -7,5 +7,8 @@ class Pessoa(object):
     def __init__(self, idade, peso, altura):
         self.dados = [idade, peso, altura]
 
-    def aniversario(self):
-        self.idade += 1
+    def engordar(self, peso_obtido):
+        self.dados[1] += peso_obtido
+
+    def emagrecer(self, peso_perdido):
+        self.dados[1] -= peso_perdido
--- 04/pessoa_spec.py
+++ 04/pessoa_spec.py
@@ -6,11 +6,19 @@ from pessoa import Pessoa
 class TestExercicio4(unittest.TestCase):
 
     def setUp(self):
-        self.pessoa = Pessoa(19, 85, 1.70)
+        self.pessoa = Pessoa(19, 85, 170)
 
     def teste_consultar_dados_de_uma_pessoa(self):
-        self.pessoa.dados |should| equal_to([19, 85, 1.70])
+        self.pessoa.dados |should| equal_to([19, 85, 170])
 
     def teste_alterar_dados_de_uma_pessoa(self):
-        self.pessoa.dados = [20, 87, 1.80]
-        self.pessoa.dados |should| equal_to([20, 87, 1.80])
+        self.pessoa.dados = [20, 87, 180]
+        self.pessoa.dados |should| equal_to([20, 87, 180])
+
+    def teste_pessoa_deve_engordar_3_quilos(self):
+        self.pessoa.engordar(3)
+        self.pessoa.dados[1] |should| equal_to(88)
+
+    def teste_pessoa_deve_emagrecer_5_quilos(self):
+        self.pessoa.emagrecer(5)
+        self.pessoa.dados[1] |should| equal_to(80)
